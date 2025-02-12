e5fe7198633fef235c5565f8762ce25c30740d80
roshan <roshanbhandari552@gmail.com>
2025-02-11 23:59:31 -0500

    first commit

---
+++ src/main/java/com/roshan/springboot/todowebapp/todo/TodoRepo.java
@@ -0,0 +1,11 @@
+package com.roshan.springboot.todowebapp.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepo extends JpaRepository<Todo, Long> {
+    List<Todo> findByUsername(String username);
+}
